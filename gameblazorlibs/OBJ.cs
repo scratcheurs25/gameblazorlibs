26507bc33bba87f3f68fbc7f9333e49815d5445f
scratcheurs25 <126276357+scratcheurs25@users.noreply.github.com>
2023-06-26 16:30:28 +0200

    Add files via upload

---
+++ gameblazorlibs/OBJ.cs
@@ -0,0 +1,56 @@
+﻿namespace mincraft.gameblazorlibs
+{
+    public class OBJ
+    {
+        public static string text_carateur(string text)
+        {
+            if (text == null)
+            {
+                return "";
+            }
+            else
+            {
+                if (text == "er")
+                {
+                    return "é";
+                }else if(text == "et")
+                {
+                    return "è";
+                }
+                else if(text == "a")
+                    {
+                    return "oi";
+                }else if(text == "o")
+                {
+                    return "au";
+                }else
+                {
+                    return "";
+                }
+            }
+        }
+        public static string decarateur(string text)
+        {
+            if (decarateur == null)
+            {
+                return "";
+            }else if(text == "é")
+            {
+                return "er";
+            }else if (text == "è")
+            {
+                return "et";
+            }else if (text == "oi")
+            {
+                return "a";
+            }else if(text == "au")
+            {
+                return "o";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
